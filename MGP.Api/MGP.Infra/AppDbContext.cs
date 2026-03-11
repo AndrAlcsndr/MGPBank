e143eb43f30b22bf3d0fd278b3d34264beb4f613
AndrAlcsndr <andralcsndr@gmail.com>
2026-03-11 01:04:15 -0300

    implem. de appDbContext

---
+++ MGP.Api/MGP.Infra/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MGP.Infra
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public void RemoveEntityContext<TEntity>(TEntity entity) where TEntity : class
+        {
+            if (entity is not null)
+                Set<TEntity>().Remove(entity);
+        }
+
+
+    }
+}
